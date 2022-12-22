a334b8151bbcc30b5b5ba5a0fb44aaf3df9219b6
Furkan Emin <femincanx@gmail.com>
2022-12-22 12:11:26 +0300

    feat: add route to add exercise

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,9 +1,11 @@
 const express = require('express');
 
-const { createUser } = require('../controllers/user.controller');
+const { createUser, addExercise } = require('../controllers/user.controller');
 
 const router = express.Router();
 
 router.post('/', createUser);
+
+router.post('/:id/exercises', addExercise);
 
 module.exports = router;
