025cfadf0d80f44caed73e050889a6d313fc8e91
Furkan Emin <femincanx@gmail.com>
2022-12-22 12:10:47 +0300

    feat: add addExercise controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -9,6 +9,33 @@ const createUser = async (req, res) => {
   }
 };
 
+const addExercise = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    const dateObj = new Date(req.body.date);
+
+    const logObject = {
+      description: req.body.description,
+      duration: parseInt(req.body.duration),
+      date: dateObj,
+    };
+
+    user.log.push(logObject);
+    user.count = user.log.length;
+    await user.save();
+
+    res.status(201).json({
+      username: user.username,
+      ...logObject,
+      date: dateObj.toDateString(),
+      _id: user._id,
+    });
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+
 module.exports = {
   createUser,
+  addExercise,
 };
