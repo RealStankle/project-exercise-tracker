3d47cd10881789f4e99b34743d7f7a37c7f0054c
Furkan Emin <femincanx@gmail.com>
2022-12-22 12:11:50 +0300

    test: add test for exercise insert

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const request = require('supertest');
 const app = require('../app');
+const User = require('../src/models/user.model');
 
 require('dotenv').config();
 
@@ -20,5 +21,26 @@ describe('POST /api/users', () => {
       .send('username=user1');
     expect(response.statusCode).toBe(201);
     expect(response.body.username).toEqual('user1');
+  });
+});
+
+describe('POST /api/users/:_id/exercises', () => {
+  it("should add an exercise to the user's logs", async () => {
+    const user = await User.findOne({ username: 'user1' });
+
+    const response = await request(app)
+      .post(`/api/users/${user.id}/exercises`)
+      .send({
+        description: 'This is a description.',
+        duration: '120',
+        date: '2012-5-13',
+      })
+      .type('form');
+
+    expect(response.statusCode).toBe(201);
+    expect(response.body.username).toEqual('user1');
+    expect(response.body.description).toEqual('This is a description.');
+    expect(response.body.date).toEqual('Sun May 13 2012');
+    expect(user.count).toBe(user.log.length);
   });
 });
