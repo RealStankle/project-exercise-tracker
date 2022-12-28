ecb695ecc489c38e76409d21c178b14335be204e
Furkan Emin <femincanx@gmail.com>
2022-12-28 17:31:33 +0300

    feat: add route to get user logs

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,11 +1,17 @@
 const express = require('express');
 
-const { createUser, addExercise } = require('../controllers/user.controller');
+const {
+  createUser,
+  addExercise,
+  getUserLogs,
+} = require('../controllers/user.controller');
 
 const router = express.Router();
 
 router.post('/', createUser);
 
 router.post('/:id/exercises', addExercise);
+
+router.get('/:id/logs', getUserLogs);
 
 module.exports = router;
