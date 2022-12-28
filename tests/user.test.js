d94bfc54eee2c762a8399e52167e92010eb2afdd
Furkan Emin <femincanx@gmail.com>
2022-12-28 17:33:12 +0300

    test: add tests to get user logs

--- tests/user.test.js
+++ tests/user.test.js
@@ -45,3 +45,79 @@ describe('POST /api/users/:_id/exercises', () => {
     expect(user.count).toBe(user.log.length);
   });
 });
+
+describe('GET /api/users/logs', () => {
+  let userId;
+  it('should return all logs of the user', async () => {
+    const {
+      body: { _id },
+    } = await request(app)
+      .post('/api/users')
+      .send({ username: 'log_test' })
+      .type('form');
+
+    userId = _id;
+
+    for (let i = 1; i <= 20; i++) {
+      await request(app)
+        .post(`/api/users/${userId}/exercises`)
+        .send({
+          description: `Description ${i}.`,
+          duration: i * 10,
+          date: `2022-10-${i}`,
+        })
+        .type('form');
+    }
+
+    const response = await request(app).get(`/api/users/${userId}/logs`);
+
+    const logs = response.body.log;
+
+    expect(response.body.username).toBe('log_test');
+    expect(logs.length).toBe(20);
+    expect(response.body.count).toBe(logs.length);
+    expect(response.body._id).toBe(userId);
+    expect(logs[0].description).toBe('Description 1.');
+    expect(logs[0].duration).toBe(10);
+    expect(logs[0].date).toBe('Sat Oct 01 2022');
+  });
+
+  it("should return logs from 'from date' parameter", async () => {
+    const response = await request(app).get(
+      `/api/users/${userId}/logs?from=2022-10-6`
+    );
+
+    const logs = response.body.log;
+
+    expect(logs.length).toBe(15);
+    expect(response.body.count).toBe(logs.length);
+    expect(logs[0].description).toBe('Description 6.');
+    expect(logs[0].duration).toBe(60);
+    expect(logs[0].date).toBe('Thu Oct 06 2022');
+  });
+
+  it("should return logs from 'from date' parameter to 'to date' parameter", async () => {
+    const response = await request(app).get(
+      `/api/users/${userId}/logs?from=2022-10-11&to=2022-10-16`
+    );
+
+    const logs = response.body.log;
+
+    expect(logs.length).toBe(5);
+    expect(response.body.count).toBe(logs.length);
+    expect(logs[0].description).toBe('Description 11.');
+    expect(logs[0].duration).toBe(110);
+    expect(logs[0].date).toBe('Tue Oct 11 2022');
+  });
+
+  it('should return logs according to the given limit', async () => {
+    const response = await request(app).get(
+      `/api/users/${userId}/logs?from=2022-10-11&to=2022-10-16&limit=2`
+    );
+
+    const logs = response.body.log;
+
+    expect(logs.length).toBe(2);
+    expect(response.body.count).toBe(logs.length);
+  });
+});
