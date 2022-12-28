604047eed438537a3ced2def8d53df9b9cd1d59d
Furkan Emin <femincanx@gmail.com>
2022-12-28 17:30:21 +0300

    feat: add getUserLogs controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -35,7 +35,38 @@ const addExercise = async (req, res) => {
   }
 };
 
+const getUserLogs = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+
+    const from = req.query.from ? new Date(req.query.from).getTime() : 0;
+    const to = req.query.to ? new Date(req.query.to).getTime() : Infinity;
+    const limit = req.query.limit ? parseInt(req.query.limit) : user.log.length;
+
+    const filteredLogs = user.log
+      .filter(
+        (exercise) =>
+          exercise._doc.date.getTime() >= from && exercise.date.getTime() < to
+      )
+      .map((exercise) => {
+        delete exercise._doc._id;
+        return { ...exercise._doc, date: exercise.date.toDateString() };
+      })
+      .slice(0, limit);
+
+    res.status(200).json({
+      username: user.username,
+      count: filteredLogs.length,
+      _id: user.id,
+      log: filteredLogs,
+    });
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+
 module.exports = {
   createUser,
   addExercise,
+  getUserLogs,
 };
