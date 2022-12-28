b7cbad9b63b8e79e5b2f87cfff11ba9ab01fc0e9
Furkan Emin <femincanx@gmail.com>
2022-12-28 17:40:43 +0300

    fix: user id format should be string in response

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,7 +3,7 @@ const User = require('../models/user.model');
 const createUser = async (req, res) => {
   try {
     const user = await User.create(req.body);
-    res.status(201).json({ username: user.username, _id: user._id });
+    res.status(201).json({ username: user.username, _id: user.id });
   } catch (error) {
     res.status(500).json(error);
   }
